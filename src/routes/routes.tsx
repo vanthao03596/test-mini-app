f672dd6dca49b7a7ebaaa3b62355f5572ff1a4c6
ntdat1306 <ntdat1306@gmail.com>
2024-07-10 10:14:01 +0700

    update: hash route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,13 +1,13 @@
 import { LoginPage } from '@/pages/LoginPage';
-import { createBrowserRouter, Navigate } from 'react-router-dom';
+import { createHashRouter, Navigate } from 'react-router-dom';
 import { ProtectedRoutes } from './ProtectedRoute';
+import cryptoRouter from './crypto';
 import earnRouter from './earn';
+import homeRouter from './home';
 import userRouter from './user';
 import walletRouter from './wallet';
-import cryptoRouter from './crypto';
-import homeRouter from './home';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: '/',
         element: <ProtectedRoutes />,
